b14a0924fc3e36751b894a638ed2e0b8569caef9
Pavel Bogachev <xxittacion@gmail.com>
2023-04-22 19:32:12 +0300

    Added sticker handler

--- main.py
+++ main.py
@@ -12,12 +12,19 @@ db = Dispatcher(bot=bot)
 # Обработчик команды start
 @db.message_handler(commands=['start'])
 async def start(message: types.Message):
-  await message.answer(f'{message.from_user.first_name}, добро пожаловать в магазин кроссовок.')
+  await message.answer_sticker('CAACAgIAAxkBAAMSZEQIb6F0CXsqitMen__vvH6Bt-QAAtgPAAJI8mBLFfvE2nh0a5gvBA') # отправить стикер
+  await message.answer(f'{message.from_user.first_name}, рад вас видеть в магазине кроссовок.')
 
-# Обработчик неизвестных сообщений и команд
+# Обработчик неизвестных команд
 @db.message_handler()
 async def answer(message: types.Message):
+  await message.answer_sticker('CAACAgIAAxkBAAMaZEQJ3EURLRqonFGl5UxeIIso1SIAArwLAAL9F5lLRQUdiUsKeUcvBA')
   await message.reply(f'{message.from_user.first_name}, я вас не понимаю!')
+
+# Обработчик стикеров, узнать id стикера: @idstickerbot
+@db.message_handler(content_types=['sticker'])
+async def check_sticker(message: types.Message):
+  await message.answer(message.sticker.file_id) # вернёт id стикера
 
 if __name__ == '__main__':
   executor.start_polling(db)
